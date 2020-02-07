ac8add489c9d1dbcc3074ee7c806e1875e4c242c
babbithaaks <60198109+babbithaaks@users.noreply.github.com>
2020-02-07 16:44:24 +0530

    small addition

--- src/Hello.js
+++ src/Hello.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Hello extends Component {
 	render(){
 		return(
+			<div>
 				<h1>Hello World</h1>
+				<p>Welcome to React</p>
+			</div>
 			)
 	}
 }
