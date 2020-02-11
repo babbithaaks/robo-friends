662c2b02ae47146f54348a2a16e5f201f4ea774c
babbithaaks <60198109+babbithaaks@users.noreply.github.com>
2020-02-11 11:17:25 +0530

    Updates

---
+++ precache-manifest.b408c05cfec2159dadf7a784efd92526.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c7256252bad24082b3217a3dfb7009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f350ac89e01f0270cfc1",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e3310505557a02b450b8",
+    "url": "/static/css/main.8b63f59d.chunk.css"
+  },
+  {
+    "revision": "f350ac89e01f0270cfc1",
+    "url": "/static/js/2.a43c4bc9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a43c4bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3310505557a02b450b8",
+    "url": "/static/js/main.f872d874.chunk.js"
+  },
+  {
+    "revision": "a79fe040d527b0f16196",
+    "url": "/static/js/runtime-main.a19a8bef.js"
+  }
+]);
